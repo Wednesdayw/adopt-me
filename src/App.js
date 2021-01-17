a4990f1cf66d14240d05f7e8dafdf0381e4895af
Wednesdayw <lolwshtf@gmail.com>
2021-01-16 19:45:43 -0500

    day 6.2

--- src/App.js
+++ src/App.js
@@ -4,30 +4,14 @@ import { render } from 'react-dom';
 import SearchParams from "./SearchParams";
 
 const App = () => {
-  // return React.createElement("div", { id: "something-important" }, [
-  //   React.createElement("h1", {}, "Adopt Me!"),
-  //   React.createElement(Pet, {
-  //     name: "Luna",
-  //     animal: "Dog",
-  //     breed: "Havanese",
-  //   }),
-  //   React.createElement(Pet, {
-  //     name: "Pepper",
-  //     animal: "Bird",
-  //     breed: "Cockatiel",
-  //   }),
-  //   React.createElement(Pet, {
-  //     name: "Doink",
-  //     animal: "Cat",
-  //     breed: "Mixed",
-  //   }),
-  // ]);
   return(
-    <div>
-      <h1 id= "something-important">Adopt Me!</h1>
-      <SearchParams />
+    <React.StrictMode>
+      <div>
+        <h1 id= "something-important">Adopt Me!</h1>
+        <SearchParams />
 
-    </div>
-  )
+      </div>
+    </React.StrictMode>
+  );
 };
 render(<App />, document.getElementById("root"));
