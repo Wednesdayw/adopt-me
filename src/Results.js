4dffeed20c578acdba4198f62bef424e7bcb0090
Wednesdayw <lolwshtf@gmail.com>
2021-01-18 21:25:58 -0500

    day 8

--- src/Results.js
+++ src/Results.js
@@ -4,16 +4,22 @@ import Pet from './Pet';
 const Results = ({ pets }) => {
     return (
         <div className = "search">
-            {pets.length === 0 ? <h1>No Pets Found</h1>
-    ): (
-        pets.map(pet => (
-            <Pet 
-            animal={pets.type}
-            key={pets.id}
-            name={pets.name}
-            breed={pets.}
+            {pets.length === 0 ? (
+                 <h1>No Pets Found</h1>
+            ) : (
+                pets.map(pet => (
+                    <Pet 
+                    animal={pets.type}
+                    key={pets.id}
+                    name={pets.name}
+                    breed={pets.breeds.primary}
+                    media={pets.photos}
+                    location={`${pet.contact.address.city}, ${
+                        pet.contact.address.state
+                    }`}
+                    id={pet.id}
 
-            //Left off here
+            
             />
 
         ))
@@ -22,3 +28,4 @@ const Results = ({ pets }) => {
         </div>
     );
 };
+export default Results;
